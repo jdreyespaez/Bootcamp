edcab3347c13d241fb69dba210a97dad06b9e35a
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-06-27 10:30:07 -0500

    Showing title on screen

--- bogota-abril-2019/final_project/juan-reyes/hn-twilight/src/components/App/index.js
+++ bogota-abril-2019/final_project/juan-reyes/hn-twilight/src/components/App/index.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { sleepMode } from '../../styles/twilight';
+// import List from 'components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Wrapper, Title } from './styles';
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={sleepMode}>
+        <div>
+          <Wrapper>
+            <Title>Hacker News Feed</Title>
+            {/* <List /> */}
+          </Wrapper>
+        </div>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
